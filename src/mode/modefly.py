8379a1acf1f5d8265722bf8c06b1873c1df9a8a3
JovialKnoll <BrettCKaplan@gmail.com>
2022-04-02 08:43:00 -0400

    star tweaks

--- src/mode/modefly.py
+++ src/mode/modefly.py
@@ -19,11 +19,14 @@ class ModeFly(jovialengine.ModeBase, abc.ABC):
     BAR_BORDER_COLOR = (105, 105, 124)
     BAR_CHARGE_SPEED = 0.001 / 8
     STAR_DELAY = 25
+    STAR_DISTANCE = SPACE_WIDTH + SPACE_BORDER * 2
     BACKGROUND_TIME = 2000
     __slots__ = (
-        '_background_sprites',
+        '_star_sprites_0',
+        '_star_sprites_1',
         '_star_timer',
         '_star_image_0',
+        '_star_image_1',
         '_charge',
     )
 
@@ -35,14 +38,16 @@ class ModeFly(jovialengine.ModeBase, abc.ABC):
             self.SPACE_WIDTH - (self.SPACE_BORDER * 2),
             self.SPACE_HEIGHT - (self.SPACE_BORDER * 2)
         )
-        self._camera.move_ip(self.SPACE_BORDER, self.SPACE_BORDER)
         self._background.fill(constants.BLACK)
-        self._background_sprites = pygame.sprite.Group()
+        self._star_sprites_0 = pygame.sprite.Group()
+        self._star_sprites_1 = pygame.sprite.Group()
         self._star_timer = self.STAR_DELAY
         self._star_image_0 = pygame.surface.Surface((3, 3)).convert()
         self._star_image_0.fill(constants.BLACK)
         self._star_image_0.fill(constants.WHITE, (0, 1, 3, 1))
         self._star_image_0.fill(constants.WHITE, (1, 0, 1, 3))
+        self._star_image_1 = pygame.surface.Surface((1, 1)).convert()
+        self._star_image_1.fill(constants.WHITE)
 
         stars = self.BACKGROUND_TIME // self.STAR_DELAY
         for s in range(stars):
@@ -57,25 +62,42 @@ class ModeFly(jovialengine.ModeBase, abc.ABC):
         pass
 
     def _makeStar(self, x):
-        star_sprite = jovialengine.AnimSprite()
-        star_sprite.image = self._star_image_0
-        star_sprite.rect = star_sprite.image.get_rect()
-        star_sprite.rect.center = (
+        star_sprite_0 = jovialengine.AnimSprite()
+        star_sprite_0.image = self._star_image_0
+        star_sprite_0.rect = star_sprite_0.image.get_rect()
+        star_sprite_0.rect.center = (
             x + random.randint(0, self.SPACE_BORDER),
             random.randint(0, self.SPACE_HEIGHT)
         )
-        star_sprite.addPosRel(
+        star_sprite_0.addPosRel(
             jovialengine.AnimSprite.Lerp,
             self.BACKGROUND_TIME,
-            (-self.SPACE_WIDTH, 0)
+            (-self.STAR_DISTANCE, 0)
         )
-        self._background_sprites.add(star_sprite)
+        self._star_sprites_0.add(star_sprite_0)
+        star_sprite_1 = jovialengine.AnimSprite()
+        star_sprite_1.image = self._star_image_1
+        star_sprite_1.rect = star_sprite_1.image.get_rect()
+        star_sprite_1.rect.center = (
+            x + random.randint(0, self.SPACE_BORDER),
+            random.randint(0, self.SPACE_HEIGHT)
+        )
+        star_sprite_1.addPosRel(
+            jovialengine.AnimSprite.Lerp,
+            self.BACKGROUND_TIME * 2,
+            (-self.STAR_DISTANCE, 0)
+        )
+        self._star_sprites_1.add(star_sprite_1)
 
     def _update(self, dt: int):
         self._star_timer -= dt
-        self._background_sprites.update(dt)
+        self._star_sprites_0.update(dt)
+        self._star_sprites_1.update(dt)
         if self._star_timer <= 0:
-            for sprite in self._background_sprites:
+            for sprite in self._star_sprites_0:
+                if not sprite.stillAnimating():
+                    sprite.kill()
+            for sprite in self._star_sprites_1:
                 if not sprite.stillAnimating():
                     sprite.kill()
             self._star_timer = self.STAR_DELAY
@@ -90,7 +112,8 @@ class ModeFly(jovialengine.ModeBase, abc.ABC):
         pass
 
     def _drawPreSprites(self, screen: pygame.surface.Surface):
-        self._background_sprites.draw(screen)
+        self._star_sprites_0.draw(screen)
+        self._star_sprites_1.draw(screen)
 
     def _drawPostSprites(self, screen: pygame.surface.Surface):
         pass
