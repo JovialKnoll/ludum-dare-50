c204acc6d95f2d90c8a105d2213fb0aed62ccfa2
JovialKnoll <BrettCKaplan@gmail.com>
2022-04-03 08:21:04 -0400

    chaotic

--- src/enemy.py
+++ src/enemy.py
@@ -26,25 +26,38 @@ class Enemy(jovialengine.AnimSprite):
         self.image = image
         self.rect = self.image.get_rect()
         self.rect.topleft = (672, random.randint(self.rect.height, 360 - self.rect.height * 2))
-        movement = random.choice(self.MOVEMENT_OPTIONS)
-        timing = random.randint(900, 1100)
         if bool(random.getrandbits(1)):
+            self.addPosRel(
+                random.choice(self.MOVEMENT_OPTIONS),
+                random.randint(900, 1100),
+                (-168, 0)
+            )
+        else:
+            self.addPosAbs(
+                random.choice(self.MOVEMENT_OPTIONS),
+                random.randint(900, 1100),
+                (504, 180)
+            )
+        self.addPosAbs(
+            random.choice(self.MOVEMENT_OPTIONS),
+            random.randint(450, 550),
+            (504, random.randint(16 + self.rect.height // 2, 360 - 16 - self.rect.height // 2)),
+            callback=self._animCallback
+        )
+
+    def _animCallback(self):
+        if self.rect.right < 0:
+            self.kill()
+        else:
+            movement = random.choice(self.MOVEMENT_OPTIONS)
+            timing = random.randint(950, 1050)
+            distance = random.randint(84, 168)
             self.addPosRel(
                 movement,
                 timing,
                 (-168, 0),
                 callback=self._animCallback
             )
-        else:
-            self.addPosAbs(
-                movement,
-                timing,
-                (504, 180),
-                callback=self._animCallback
-            )
-
-    def _animCallback(self):
-        pass
 
     def update(self, *args):
         super().update(args[0])
