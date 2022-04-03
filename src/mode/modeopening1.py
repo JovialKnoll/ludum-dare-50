b2e0bc1b8be03ea12cb3d1dff264fc502df3cb29
JovialKnoll <BrettCKaplan@gmail.com>
2022-04-03 19:26:05 -0400

    fancier title screen

--- src/mode/modeopening1.py
+++ src/mode/modeopening1.py
@@ -2,6 +2,7 @@ import pygame
 import jovialengine
 
 import constants
+import gameutility
 import mode
 
 
@@ -21,12 +22,35 @@ class ModeOpening1(mode.ModeOpening):
         )
         title_font = pygame.font.Font(constants.TITLE_FONT, int(constants.FONT_SIZE * 1.5))
         title_font_wrap = jovialengine.FontWrap(title_font, int(constants.FONT_HEIGHT * 1.5), False)
-        title_font_wrap.renderToCentered(
-            self._background,
-            (constants.SCREEN_SIZE[0] // 2, constants.SCREEN_SIZE[1] // 4),
+        title_surf = title_font_wrap.renderInside(
+            title_font.size(constants.TITLE)[0],
             constants.TITLE,
             constants.WHITE,
             constants.BLACK
+        )
+        for i in range(len(constants.TITLE)):
+            x = title_font.size(constants.TITLE[:i])[0]
+            if i in (4, 13, 14):
+                x -= 1
+            elif i == 11:
+                x -= 2
+            elif i == 5:
+                x -= 3
+            elif i == 15:
+                x -= 5
+            title_font_wrap.renderToInside(
+                title_surf,
+                (x, 0),
+                title_font.size(constants.TITLE[i])[0],
+                constants.TITLE[i],
+                gameutility.getBarColor(i / len(constants.TITLE))
+            )
+        title_surf_rect = title_surf.get_rect()
+        title_surf_rect.center = (constants.SCREEN_SIZE[0] // 2, constants.SCREEN_SIZE[1] // 4)
+        pygame.Surface.blit(
+            self._background,
+            title_surf,
+            title_surf_rect
         )
         version_width = jovialengine.shared.font_wrap.font.size(constants.VERSION)[0]
         jovialengine.shared.font_wrap.renderTo(
